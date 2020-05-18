77cdf60eba76f5b18d682cb70f46b41db67d38dc
Michael Schmidt <mschmidt@e1z1r5p13.42.us.org>
2020-05-18 00:49:44 -0700

    trying a few things

--- ft_printf.c
+++ ft_printf.c
@@ -5,134 +5,106 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mschmidt <mschmidt@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/05/12 19:48:31 by mschmidt          #+#    #+#             */
-/*   Updated: 2020/05/15 19:49:36 by mschmidt         ###   ########.fr       */
+/*   Created: 2020/05/15 19:53:47 by mschmidt          #+#    #+#             */
+/*   Updated: 2020/05/17 02:39:11 by mschmidt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include "./libft/libft.h"  // FIX THIS TRASH!!
+#include "./libft/libft.h" // FIX THIS TRASH!!
 
-/*
-void	printf_conv_flags(const char **arg, const char flag)
-{
-	
-}
-*/
+void	left_adjust(t_list **lst);
+void	zeropad_adjust(t_list **lst);
+void	width_adjust(t_list **lst);
+void	precision_adjust(t_list **lst);
+t_list	*conv_stack(void *param, char conv);
 
-void	ft_lstprint(t_list *lst, char *delimiter)
+void	ft_lstprint(t_list *lst, const char *delim)
 {
-	while (lst != NULL)
+	t_list	*temp;
+
+	temp = lst;
+	while (temp)
 	{
-		ft_putstr_fd(lst->content, 1);
-		ft_putstr_fd(delimiter, 1);
-		lst = lst->next;
+		ft_putstr_fd(temp->content, 1);
+		ft_putstr_fd(delim, 1);
+		temp = temp->next;
 	}
+
 }
 
-char	*printf_parser(const char *str, t_list **stack)
+void	printf_parser(t_list **stack, const char *str)
 {
 	int		i;
 	const char	convchar[9];
-	int		start;
 
-	if (!str)
-		return (0);
 	i = 1;
-	start = 0;
 	ft_strlcpy((char*)convchar, "cspdiuxX", 9);
-	ft_putstr_fd("parser str: ", 1);
+	// CHECK INPUT
+	ft_putstr_fd("parser input str: ", 1);
 	ft_putstr_fd(str, 1);
-	while (!ft_strchr(convchar, str[i]))
+	ft_putstr_fd("\n", 1);
+	////////
+	while (!ft_strchr(convchar, str[i]) && str[i] != '\0')
 	{
-		if (ft_isdigit(str[i]))
-		{
-			if(ft_isdigit(str[i + 1]) && start == 0)
-				start = i;
-		}
-		else if (start > 0 && !ft_isdigit(str[i]))
-		{
-			ft_lstadd_front(stack, ft_lstnew(ft_substr(str, start, (i - start))));
-			ft_lstadd_front(stack, ft_lstnew(ft_substr(&str[i], 0, 1)));
-			start = 0;
-		}
-		else
-			ft_lstadd_front(stack, ft_lstnew(ft_substr(&str[i], 0, 1)));
+		ft_lstadd_front(stack, ft_lstnew(ft_substr(&str[i], 0, 1)));
 		i++;
-		ft_lstprint(*stack, "");
-		ft_putstr_fd("\n", 1);
 	}
 	ft_lstadd_front(stack, ft_lstnew(ft_substr(&str[i], 0, 1)));
-	ft_lstprint(*stack, " ");
-	ft_putstr_fd("\n", 1);
-	return(ft_substr(str, 0, (i + 1)));
 }
 
-char	*printf_replace_star(const char *pars, int num)
+t_list	*ft_lstfind(t_list *lst, void *content)
 {
-	int		i;
-	char	*str;
-	char	**split;
-
-	i = 0;
-	split = ft_split(pars, '*');
-	str = ft_strjoin(split[0], ft_itoa(num));
-	str = ft_strjoin(str, split[1]);
-	free(split[0]);
-	free(split[1]);
-	return (str);
+	while (lst)
+	{
+		if (ft_strncmp(lst->content, content, ft_strlen(content)))
+			return (lst);
+		lst = lst->next;
+	}
+	return (NULL);
 }
 
+void	ft_lstreplaceone(t_list **lst, void *old, void *new)
+{
+	t_list	*curr;
+
+	curr = *lst;
+	while (curr)
+	{
+		if (ft_strncmp(curr->content, old, ft_strlen(old)))
+		{
+			curr->content = new;
+		}
+	}
+}
 int		ft_printf(const char *str, ...)
 {
-	int				idx;
-	va_list			args;
-	//unsigned char	c;
-	//char			*i;
-	char			*temp;
-	t_list			*f_stack;
+	int		idx;
+	t_list	*stack;
+	char	*flags;
+	char	*width;
+	char	*precision;
+	char	*conv;
+	va_list	args;
 
 	idx = 0;
+	stack = NULL;
 	va_start(args, str);
-	ft_putstr_fd("input string: ", 1);
-	ft_putstr_fd(str, 1);
-	ft_putstr_fd("\n", 1);
 	while (str[idx] != '%')
 		idx++;
-	temp = printf_parser(&str[idx], &f_stack);
-	f_stack = NULL;
-
-	//if (ft_strchr(temp, '*'))
-	//	temp = printf_replace_star(temp, va_arg(args, int)); // Memory Leak here
-	//if (ft_strchr(temp, '*'))
-	//	temp = printf_replace_star(temp, va_arg(args, int)); // Memory Leak here
-	
-	idx = 0;
-	/*
-	while (str[idx] != '\0')
+	printf_parser(&stack, &str[idx]);
+	ft_lstprint(stack, "");
+	ft_putstr_fd("\n", 1);
+	if (ft_lstfind(stack, "*"))
 	{
-
-		if (str[idx] == '%')
-		{
-			idx++;
-			if (str[idx] == 'c')
-			{
-				c = va_arg(args, int);
-				write (1, &c, 1);
-			}
-			else if (str[idx] == 'i')
-			{
-				idx++;
-				i = ft_itoa(va_arg(args, int));
-				ft_putstr_fd(i, 1);
-				ft_putstr_fd("\n", 1);
-			}
-		}		
-		i++;
+		ft_putstr_fd("found a *", 1); //REMOVE
+		ft_putstr_fd("\n", 1);// REMOVE
+		ft_lstreplaceone(&stack, "*", &va_arg(args, int));
 	}
-	*/
-	va_end(args);
-	return (idx);
+	ft_lstprint(*stack, "");
+	ft_putstr_fd("\n", 1);
+
+	return (0);
 }
